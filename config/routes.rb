e162c6b7e17c8f862dfc3c35252173a29282c0cd
erinworth <eeworth@gmail.com>
2017-09-27 12:12:27 -0700

    Assignment 11: completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   root 'welcome#index'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
