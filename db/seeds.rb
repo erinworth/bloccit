ff92a5aa970c042959d724565fa235302d8c1f29
erinworth <eeworth@gmail.com>
2017-09-30 11:53:18 -0700

    Assignment 16: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,14 @@ require 'random_data'
    )
  end
 
+ puts "#{Post.count}"
+ Post.find_or_create_by(title: "A unique title", body: "A unique body")
+ puts "#{Post.count}"
+
+ puts "#{Comment.count}"
+ Comment.find_or_create_by(title: "A unique title", body: "A unique body")
+ puts "#{Comment.count}"
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
