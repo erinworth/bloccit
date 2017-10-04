603985877d757e0e9cfc64c350db3f19f161e048
erinworth <eeworth@gmail.com>
2017-10-04 13:13:55 -0700

    assignment 19: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :advertisements
 
   resources :posts
 
   get 'about' =>'wecome#about'
 
-  root 'welcome#index'
 
 end
---
+++ spec/views/advertisement/create.html.erb_spec.rb
@@ -0,0 +1,2 @@
+<h1>Advertisements</h1>
+  <p>Find me in app/views/advertisements/create.html.erb</p>
