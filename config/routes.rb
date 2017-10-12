220bff8cade71e6ab51c5bee09e0ee21249293e4
erinworth <eeworth@gmail.com>
2017-10-12 11:02:41 -0700

    Assignment 24: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
 resources :users, only: [:new, :create]
+
 resources :topics do
   resources :posts, except: [:index]
-  resources :sponsored_posts, except: [:index]  
+  resources :sponsored_posts, except: [:index]
   resources :questions
 end
-
+  post 'users/confirm' => 'users#confirm'
   get 'about' =>'welcome#about'
 
   root 'welcome#index'
