57364d4bb1b35c6db1aed593f2736bad88d75de6
erinworth <eeworth@gmail.com>
2017-10-25 18:11:26 -0700

    checkpoint 30 completed

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ resources :topics do
 end
 
 resources :posts, only: [] do
-resources :comments, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
+
+  post '/up-vote' => 'votes#up_vote', as: :up_vote
+  post '/down-vote' => 'votes#down_vote', as: :down_vote
 end
 
   post 'users/confirm' => 'users#confirm'
