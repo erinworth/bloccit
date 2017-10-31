178f7f8376f9616c61049036ccb5536a58cd0e1b
erinworth <eeworth@gmail.com>
2017-10-31 11:15:07 -0700

    Checkpoint 32: Completed

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TopicsController, type: :controller do
-  let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:my_topic) { create(:topic) }
 
   describe "GET index" do
     it "returns http success" do
@@ -120,5 +120,5 @@ RSpec.describe TopicsController, type: :controller do
       expect(response).to redirect_to topics_path
     end
   end
-    
+
 end
